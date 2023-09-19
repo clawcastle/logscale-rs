44cc55f629d1eff88105a3a7c6e99904c8a3c86a
sunero4 <anderskloborg@gmail.com>
2023-09-19 15:48:36 +0000

    Starts adding unstructured logger. Still needs a lot of cleanup since there is a lot of duplicated code a across the two logger implementations.

--- logscale-log/src/logscale_unstructured_logger.rs
+++ logscale-log/src/logscale_unstructured_logger.rs
@@ -1,3 +1,79 @@
+use std::{error::Error, sync::{Arc, Mutex}, cell::RefCell, time::Duration};
+
+use log::Log;
+use logscale_client::{client::LogScaleClient, models::unstructured_logging::UnstructuredLogsIngestRequest};
+
+use crate::{options::{LoggerIngestPolicy, LoggerOptions}, log_events_cache::LogsEventCache, ingest_job::start_background_ingest_job};
+
 pub struct LogScaleUnstructuredLogger {
+    client: LogScaleClient,
+    options: LoggerOptions,
+    log_events_cache: Arc<Mutex<RefCell<LogsEventCache>>>,
+}
+
+impl LogScaleUnstructuredLogger {
+    pub fn init(
+        url: String,
+        ingest_token: String,
+        options: LoggerOptions,
+    ) -> Result<(), Box<dyn Error>> {
+        let logscale_logger = LogScaleUnstructuredLogger::create(&url, &ingest_token, options)?;
+
+        if let LoggerIngestPolicy::Periodically(duration) =
+            logscale_logger.options.ingest_policy
+        {
+            logscale_logger.start_periodic_sync(duration);
+        }
+
+        Ok(())
+    }
+
+    fn create(
+        url: &str,
+        ingest_token: &str,
+        options: LoggerOptions,
+    ) -> Result<Self, Box<dyn Error>> {
+        let client = LogScaleClient::from_url(url, String::from(ingest_token))?;
+
+        Ok(Self {
+            client,
+            options,
+            log_events_cache: Arc::from(Mutex::from(RefCell::new(LogsEventCache::new()))),
+        })
+    }
+
+    fn start_periodic_sync(&self, duration: Duration) {
+        let cloned_client = self.client.clone();
+        let cloned_cache = Arc::clone(&self.log_events_cache);
+
+        start_background_ingest_job(duration, &cloned_client, cloned_cache);
+    }
+}
+
+impl Log for LogScaleUnstructuredLogger {
+    fn enabled(&self, _: &log::Metadata) -> bool {
+        true
+    }
+
+    fn log(&self, record: &log::Record) {
+        match self.options.ingest_policy {
+            LoggerIngestPolicy::Immediately => {
+                if let Some(log_message) = record.args().as_str() {
+                    let client = self.client.clone();
+                    tokio::spawn(async move {
+                        let request_content = [log_message];
+                        let request = UnstructuredLogsIngestRequest::from_log_events(&request_content);
     
+                        let _ = client.ingest_unstructured(&[request]).await;
+                    });
+                }
+
+            },
+            LoggerIngestPolicy::Periodically(_) => todo!(),
+        }
+    }
+
+    fn flush(&self) {
+        
+    }
 }
