e63bea910296a1cdc64e56221f5e242db3ef4074
sunero4 <anderskloborg@gmail.com>
2023-10-02 20:50:07 +0000

    Add thiserror as dependency, remove patch version from dependencies.

--- logscale-client/src/models/errors.rs
+++ logscale-client/src/models/errors.rs
@@ -1,5 +1,11 @@
-#[derive(Debug)]
+use thiserror::Error;
+
+
+#[derive(Debug, Error)]
 pub enum LogScaleError {
+    #[error("An error occurred while trying to send the request to LogScale.")]
     ConnectionError,
+    #[error("LogScale returned a message with an HTTP status code indicating an error. Status code: {0}.")]
     ErrorWithStatusCode(u16),
 }
+
